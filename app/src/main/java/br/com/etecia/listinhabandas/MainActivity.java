99793b0d177ce8292892aaaad617529537c09a66
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2023-02-14 20:02:34 -0300

    PAIN

--- app/src/main/java/br/com/etecia/listinhabandas/MainActivity.java
+++ app/src/main/java/br/com/etecia/listinhabandas/MainActivity.java
@@ -6,17 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView ListaBandas;
+
+    int banda[] = {R.drawable.starset,R.drawable.imaginedragoes};
+    String nome[] = {"Starset","Imagine Dragons"};
+    String integrantes[] = {"Dustin Bates, Ron DeChant, Brock Richards, Adam Gilbert","Umas pessoas legais"};
+    String genero[] = {"Rock Alternativo","Rock"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ListaBandas = findViewById(R.id.idlistaBandas);
         MyAdapter adapter = new MyAdapter();
+
         ListaBandas.setAdapter(adapter);
     }
 
@@ -40,7 +49,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+
+            ImageView imgbanda;
+            TextView txtnome, txtintegrantes, txtgenero;
+
+            View v = getLayoutInflater().inflate(R.layout.modelo_bandas,null);
+
+            imgbanda = v.findViewById(R.id.imgBanda);
+            txtnome = v.findViewById(R.id.txtNome);
+            txtintegrantes = v.findViewById(R.id.txtIntegrantes);
+            txtgenero = v.findViewById(R.id.txtGenero);
+
+            imgbanda.setImageResource(banda[i]);
+            txtnome.setText(nome[i]);
+            txtintegrantes.setText(integrantes[i]);
+            txtgenero.setText(genero[i]);
+
+
+            return v;
         }
     }
 }
